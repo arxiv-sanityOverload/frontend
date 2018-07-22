65b00a04d8f7e458c76acb755ffde362ac872210
mr-woot <tusharmudgal3@gmail.com>
2018-07-23 01:32:42 +0530

    basic config for eslint

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, createStore } from "redux";
+
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import promiseMiddleware from "redux-promise-middleware";
+import reducer from "./reducers";
+
+const middleware = applyMiddleware(promiseMiddleware(), thunk, logger);
+
+export default createStore(reducer,
+    middleware);
